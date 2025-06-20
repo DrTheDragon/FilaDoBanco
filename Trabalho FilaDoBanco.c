61bcd07a17b73b5d515e44e5f25d1fd754db9dd2
phmnsx <lynnwastinghertime@gmail.com>
2025-06-20 18:51:04 -0300

    Unificar filas e adição de fila preferencial
    Renomeei algumas variaveis, e adaptei o codigo pra receber filas de prioridade e tambem fiz uma função pra unificar as filas

--- Trabalho FilaDoBanco.c
+++ Trabalho FilaDoBanco.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h> 
 
-	typedef struct no{
-    int bollPessoa, hora, min; //no boleano 0 é pessoa física, e 1 é pessoa jurídica
-    char nome[20];
+typedef struct no{
+    int boolPessoa, boolPrioridade, hora, minuto; //no boleano 0 é pessoa física, e 1 é pessoa jurídica 
+    char nome[40];
     struct no *proxNo;
 }tipoNo;
 
@@ -14,99 +14,172 @@ typedef struct listaGerenciada{
     int quant;
 }tipoLista;
 
-void inicializa(tipoLista *lista){
-    lista->fim = NULL;
-    lista->inicio = NULL;
-    lista->quant = 0;
+void inicializa(tipoLista *listaEnc){
+    listaEnc->fim = NULL;
+    listaEnc->inicio = NULL;
+    listaEnc->quant = 0;
 }
-    
-int insereNaFila(tipoLista *listaEnc, int pessoa, char* nome, int hora, int min){
-tipoNo *novoNo;
-    novoNo = (tipoNo*)malloc(sizeof(tipoNo));
-    if(novoNo==NULL)
-        return 0;
-    novoNo->bollPessoa= pessoa;
+void imprimeSenha(tipoNo* cliente){ //gera e mostra senha baseada nos dados do cliente "cliente"
+	printf("\n");
+	if(cliente->boolPrioridade){
+		printf("PRIO");
+	}
+	printf("%d%d", cliente->hora, cliente->minuto);
+	if(!(cliente->boolPessoa))
+		printf("PF");
+	else
+		printf("PJ");
+	printf("%s", cliente->nome);
+}
+
+
+int insereNaFila(tipoLista *listaEnc, int pessoa, int prioridade, char* nome, int hora, int min){
+	tipoNo *novoNo;
+	novoNo = (tipoNo*)malloc(sizeof(tipoNo));
+	if(novoNo==NULL)
+			return 0;
+			
+	novoNo->boolPessoa= pessoa;
 	strcpy (novoNo->nome, nome);
-    novoNo->hora= hora;
-    novoNo->min = min;
-    novoNo->proxNo = NULL;
-    if (listaEnc ->fim == NULL){
+	novoNo->hora= hora;
+	novoNo->minuto = min;
+	novoNo->boolPrioridade = prioridade;
+	novoNo->proxNo = NULL;
+	if (listaEnc ->fim == NULL){
 		listaEnc->inicio = novoNo;
 		listaEnc->fim = novoNo;
 	}else{
-    listaEnc->fim->proxNo = novoNo;
-    listaEnc->fim = novoNo;
-}
-   listaEnc->quant++;
-    return 1;
-}
-
-int previsaoDaFila(tipoLista *listaEnc){
-tipoNo *atual; 
-if(listaEnc->inicio == NULL)
-    return 0;
-atual = listaEnc->inicio;
-while(atual !=NULL){
-    puts (atual->nome);
-    atual = atual->proxNo;
-    }
-    return 1;
+		listaEnc->fim->proxNo = novoNo;
+		listaEnc->fim = novoNo;
+	}
+	listaEnc->quant++;
+	return 1;
 }
 
-	int main(){
-    tipoLista filaPF, filaPJ; 
-    int op, op2, hr, mt;
-    char nm[20];
+void unificarFilas(tipoLista* listaUm, tipoLista* listaDois){
+	tipoLista listaNova;
+	inicializa(&listaNova);
+	tipoNo* proxNo, *atualUm, *atualDois;
+	atualUm = listaUm->inicio;
+	atualDois = listaDois->inicio;
+	while(atualUm != NULL || atualDois != NULL){
+		if (atualUm == NULL){
+			proxNo = atualDois;
+			atualDois = atualDois->proxNo;
+		} 
+		else if (atualDois == NULL){
+			proxNo = atualUm;
+			atualUm = atualUm->proxNo;
+		}
+		else if (atualUm->hora*60 + atualUm->minuto < atualDois->hora*60 + atualDois->minuto){
+			proxNo = atualUm;
+			atualUm = atualUm->proxNo;
+		} else {
+			proxNo = atualDois;
+			atualDois = atualDois->proxNo;
+		}
+		insereNaFila(&listaNova, proxNo->boolPessoa, proxNo->boolPrioridade, proxNo->nome, proxNo->hora, proxNo->minuto);
+	}
+}
+
+int previsaoDaFila(tipoLista *listaNaoPreferencial, tipoLista *listaPreferencial){
+	tipoNo *atual; 
+	tipoNo *atualPref;
+	if(listaNaoPreferencial->inicio == NULL && listaPreferencial->inicio == NULL)
+		return 0;
+		
+	atualPref = listaPreferencial->inicio;
+	atual = listaNaoPreferencial->inicio;
+	int clientesPAtendidos = 0;
+	while(atual != NULL || atualPref != NULL){
+		if((atualPref == NULL || clientesPAtendidos == 3) && atual != NULL){ //Mostrar nao preferencial
+			imprimeSenha(atual);
+			clientesPAtendidos = 0;
+			atual = atual->proxNo;
+		} else { 															//Mostrar preferencial
+			imprimeSenha(atualPref);
+			clientesPAtendidos++;
+			atualPref = atualPref->proxNo;
+		}
+	}
+	return 1;
+}
+
+int main(){
+	tipoLista filaPF, filaPJ, filaPPF, filaPPJ; //Declara as quatro filas (PF, PJ) x (Preferencial, Não Preferencial) 
+    int opcao, opcao2, opcao3, boolFilaUnificada = 0, hora, minuto;
+    char nome[20];
     inicializa(&filaPF);
     inicializa(&filaPJ);
+    inicializa(&filaPPF);
+    inicializa(&filaPPJ);
     do{
         printf("\n      Menu Fila do Banco");
         printf("\n1 - Adicionar pessoa na fila ");
         printf("\n2 - Previsao de Atendimento");
-        printf("\n3 - ");
+        printf("\n3 - Unificar Filas");
         printf("\n4 - ");
         printf("\n5 - ");
         printf("\n6 - ");
-         printf("\n7 - ");
+        printf("\n7 - ");
         printf("\n9 - ");
-        printf("\n0- Encerrar o programa ");
+        printf("\n0 - Encerrar o programa ");
         printf("\nDigite sua opcao:");
-        scanf("%d",&op);
-        switch(op){
-			
+        scanf("%d",&opcao);
+        switch(opcao){
+		
         case 1:
-			printf("\nqual fila que deseja inserir?");
-			printf("\nPessoa fisica = 1");
-			printf("\nPessoa juridica = 2\n");
-            scanf("%d",&op2);
+			if (!boolFilaUnificada){ //se a fila não estiver unificada, nao pergunte qual o tipo de fila que será inserido
+				printf("\nQual fila que deseja inserir?\nPessoa fisica = 1\nPessoa juridica = 2\n");
+				scanf("%d",&opcao2);
+			}
+            printf("\nA pessoa tem prioridade?\n0 - Nao\n1 - Sim\n");
+            scanf("%d",&opcao3);
             printf("\nQual nome da pessoa?\n");
             getchar();
-			fgets (nm, sizeof(nm), stdin);
+			fgets (nome, sizeof(nome), stdin);
             printf("\nEm qual tempo chegou");
             printf("\n(horas):(minutos)\n");
-            scanf ("%d:%d", &hr, &mt);
-				if (op2 ==1){
-				insereNaFila (&filaPF, 0, nm, hr, mt); 
-			}else if (op2 ==2){
-				insereNaFila (&filaPJ, 1, nm, hr, mt); 
+            scanf ("%d:%d", &hora, &minuto);
+            
+			if (opcao2 == 1 || boolFilaUnificada){ //Escolhe a fila para botar a pessoa
+				if (opcao3){
+					insereNaFila (&filaPPF, 0, opcao3, nome, hora, minuto);
+				}else{
+					insereNaFila (&filaPF, 0, opcao3, nome, hora, minuto);
+				}
+			}else if (opcao2 == 2){
+				if (opcao3){
+					insereNaFila (&filaPPJ, 1, opcao3, nome, hora, minuto);
+				}
+				else{
+					insereNaFila (&filaPJ, 1, opcao3, nome, hora, minuto); 
+				}
 			}
            break;
             
             
         case 2:
-			printf("\nqual fila que deseja analisar?");
+			printf("\nQual fila que deseja analisar?");
 			printf("\nPessoa fisica = 1");
 			printf("\nPessoa juridica = 2\n");
-			scanf ("%d", &op2);
-			if (op2 ==1){
-				printf("\nfila do caixa 1:\n");
-				previsaoDaFila (&filaPF); 
-			}else if (op2 ==2){
-				printf("\nfila do caixa 2:\n");
-				previsaoDaFila (&filaPJ); 
+			scanf ("%d", &opcao2);
+			if (opcao2 ==1){
+				printf("\nFila do caixa 1:\n");
+				previsaoDaFila (&filaPF, &filaPPF);
+			}else if (opcao2 ==2){
+				printf("\nFila do caixa 2:\n");
+				previsaoDaFila (&filaPJ, &filaPPJ);
 			}
              break;
         case 3:
+			 if(boolFilaUnificada){
+				 printf("\nFilas ja unificadas!\n");
+				 break;
+			 }
+			 unificarFilas(&filaPF, &filaPJ);
+			 unificarFilas(&filaPPF, &filaPPJ);
+			 boolFilaUnificada++;
              break;
         case 4: 
 			 break;
@@ -122,10 +195,11 @@ while(atual !=NULL){
         case 0: printf("\nEncerrando programa.");
             break;
         default: printf("\nOpção inválida!");
+				 getchar();
         }
      printf("\n\n\n\n");
-    }while(op != 0);
-    if (op == 0){
+    }while(opcao != 0);
+    if (opcao == 0){
 		return 0;
 	}
 }
